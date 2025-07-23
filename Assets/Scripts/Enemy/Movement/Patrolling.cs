0c9e2b225d74cfb058d422e48cd9e053b692871a
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-07-23 12:55:57 +0300

    Make animations dependant from speed. Add run animation.

--- Assets/Scripts/Enemy/Movement/Patrolling.cs
+++ Assets/Scripts/Enemy/Movement/Patrolling.cs
@@ -1,21 +1,22 @@
 using System;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
 namespace Enemy.Movement {
     public class Patrolling : MonoBehaviour {
-        [SerializeField] private NavMeshAgent agent;
-        [SerializeField] private Transform[] points;
-        [SerializeField] private Vector2 idleTime;
-        [SerializeField] private PatrolType patrolType;
-        [SerializeField] private float offsetToDestination;
+        [SerializeField] private NavMeshAgent _agent;
+        [SerializeField] private Transform[] _points;
+        [SerializeField] private Vector2 _idleTime;
+        [SerializeField] private Vector2 _moveSpeed;
+        [SerializeField] private PatrolType _patrolType;
+        [SerializeField] private float _offsetToDestination;
         [Space]
-        [SerializeField] private Animator animator;
-        [SerializeField] private float animationCrossFade = 0.2f;
-        [SerializeField] private AnimationClip idleAnimation;
-        [SerializeField] private AnimationClip walkAnimation;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private string _animatorSpeedPeramName;
+        private int _speedParamId;
 
         private bool _isIdle;
         private int _currentCircleIndex;
@@ -24,50 +25,58 @@ namespace Enemy.Movement {
 
         private Vector3 _nextDestination;
 
-        private void Awake() => _flipFlopIndexIncrement = 1;
+        private void Awake() {
+            _flipFlopIndexIncrement = 1;
+            _speedParamId = _animator.parameters.First(p => p.name == _animatorSpeedPeramName)
+                .nameHash;
+        }
 
         private void Update() {
+            _animator.SetFloat(_speedParamId, _agent.velocity.magnitude);
             // This probably can be placed in coroutine, but it may open a memory leak due to recursiveness...
             if (ReachedDestination() && !_isIdle) StartCoroutine(SetNextDestinationLater());
         }
 
         private Vector3 NextRandomDestination() {
-            return points[Random.Range(0, points.Length)].position;
+            return _points[Random.Range(0, _points.Length)].position;
         }
 
         private Vector3 NextCircleDestination() {
-            var nextPoint = points[_currentCircleIndex].position;
-            
+            var nextPoint = _points[_currentCircleIndex].position;
+
             _currentCircleIndex++;
-            if (_currentCircleIndex >= points.Length) _currentCircleIndex = 0;
+            if (_currentCircleIndex >= _points.Length) _currentCircleIndex = 0;
             _currentFlipFlopIndex = _currentCircleIndex;
 
             return nextPoint;
         }
 
         private Vector3 NextFlipFlopDestination() {
-            var nextPoint = points[_currentCircleIndex].position;
-            
+            var nextPoint = _points[_currentCircleIndex].position;
+
             _flipFlopIndexIncrement += _flipFlopIndexIncrement;
-            if (_flipFlopIndexIncrement >= points.Length) {
+            if (_flipFlopIndexIncrement >= _points.Length) {
                 _flipFlopIndexIncrement = -1;
                 _currentFlipFlopIndex -= 2;
+
                 // when only one point
                 if (_currentFlipFlopIndex < 0) _currentFlipFlopIndex = 0;
             }
             else if (_currentFlipFlopIndex < 0) {
                 _flipFlopIndexIncrement = 1;
                 _currentFlipFlopIndex += 2;
+
                 // when only one point
-                if (_currentFlipFlopIndex >= points.Length) _currentFlipFlopIndex = 0;
+                if (_currentFlipFlopIndex >= _points.Length) _currentFlipFlopIndex = 0;
             }
+
             _currentCircleIndex = _currentFlipFlopIndex;
 
             return nextPoint;
         }
 
         private Vector3 NextDestination() {
-            return patrolType switch {
+            return _patrolType switch {
                 PatrolType.Random => NextRandomDestination(),
                 PatrolType.FlipFlop => NextFlipFlopDestination(),
                 PatrolType.Circle => NextCircleDestination(),
@@ -76,18 +85,15 @@ namespace Enemy.Movement {
         }
 
         private IEnumerator SetNextDestinationLater() {
-            animator.CrossFade(idleAnimation.name, animationCrossFade);
             _isIdle = true;
-            
-            yield return new WaitForSeconds(Random.Range(idleTime.x, idleTime.y));
-            agent.SetDestination(NextDestination());
-
+            yield return new WaitForSeconds(Random.Range(_idleTime.x, _idleTime.y));
+            _agent.speed = Random.Range(_moveSpeed.x, _moveSpeed.y);
+            _agent.SetDestination(NextDestination());
             _isIdle = false;
-            animator.CrossFade(walkAnimation.name, animationCrossFade);
         }
 
         private bool ReachedDestination() {
-            return Vector3.Distance(agent.destination, transform.position) < offsetToDestination;
+            return Vector3.Distance(_agent.destination, transform.position) < _offsetToDestination;
         }
     }
 }
